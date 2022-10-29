7ac8bd36980a776f7a6e3025585bb3a7c4bceaab
Normand-1024 <hawtherabbit@gmail.com>
2022-10-29 15:36:05 -0700

    added selected/filtered for bubble view, some basic selection logic for bubble view

---
+++ constants.js
@@ -0,0 +1,35 @@
+const UNI_COLORS = {
+    'University of California-Berkeley': '#ffcc00',
+    'University of California-Davis': '#ff6666',
+    'University of California-Irvine': '#cc0066',
+    'University of California-Los Angeles': '#66cccc',
+    'University of California-Merced': '#f688bb',
+    'University of California-Berkeley': '#65587f',
+    'University of California-Riverside': '#baf1a1',
+    'University of California-San Diego': '#0c7b93',
+    'University of California-Santa Barbara': '#75b79e',
+    'University of California-Santa Cruz': '#9de3d0'
+}
+
+const DEPART_COLORS = {
+    "AGRICULTURE, NATURAL RESOURCES AND ENVIRONMENTAL STUDIES": '#d175ff',
+    "ARCHITECTURAL AND ENVIRONMENTAL DESIGN AND PLANNING": '#ffb3b7',
+    "ART—APPLIED, FINE, PERFORMING AND ART HISTORY": '#ff6666',
+    "BIOLOGICAL AND LIFE SCIENCES": '#cc0066',
+    "BUSINESS AND MANAGEMENT": '#66cccc',
+    "COMMUNICATION": '#f688bb',
+    "ENGINEERING AND COMPUTER SCIENCE": '#65587f',
+    "ETHNIC AND AREA STUDIES": '#baf1a1',
+    "FOOD, NUTRITION AND CONSUMER SCIENCES": '#0c7b93',
+    "FOREIGN LANGUAGE AND LITERATURE": '#75b79e',
+    "HEALTH PROFESSIONS": '#befaea',
+    "HUMANITIES": "#7cd461",
+    "MATHEMATICS": "#e7f0af",
+    "PHYSICAL SCIENCES": "#f59af3",
+    "SOCIAL SCIENCES": "#c48997",
+    "SPECIAL PROGRAMS": "#aff0e0"
+}
+
+const EVENT_CODE = {
+    "bubble_click_update": 0
+}
